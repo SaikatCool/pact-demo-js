c7e7673b1139e1c9e5e8017b8f21f605c9571d16
z00303mf <saikat.gupta@siemens.com>
2020-05-24 21:31:47 +0530

    Pact broker changes

--- provider/pact.spec.js
+++ provider/pact.spec.js
@@ -8,10 +8,11 @@ describe("Pact Verification", () => {
             logLevel: "INFO",
             providerBaseUrl: "http://localhost:3000",
             provider: "dogservice",
-            providerVersion: "1.0.0",
-            pactUrls: [
-                __dirname + '/pacts/ui-dogservice.json'
-            ]
+            providerVersion: "0.0.1",
+            pactBrokerUrl: process.env.pact_broker_url || "http://localhost:8081",
+            pactBrokerUsername: process.env.pact_broker_username || 'pact_workshop',
+            pactBrokerPassword: process.env.pact_broker_password || 'pact_workshop',
+            publishVerificationResult: true
         };
 
         return new Verifier(opts).verifyProvider().then(output => {
